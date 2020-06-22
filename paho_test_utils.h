bdd02d47f2089442dfd26c64779dde9fc74703c6
jie-UX32A <jieyng@umich.edu>
2020-06-22 09:19:34 -0400

    added stuff into path_test_utils.cpp/.h
    Signed-off-by: jie-UX32A <jieyng@umich.edu>

--- paho_test_utils.h
+++ paho_test_utils.h
@@ -1,11 +1,19 @@
 #ifndef PAHO_TEST_UTILS_H
 #define PAHO_TEST_UTILS_H
 
+#include <iostream>
+#include <QMessageBox>
+#include <stdio.h>
+#include <stdlib.h>
+#include <QString>
+#include <string>
+#include <TVF_To_DXP.pb.h>
+#include <TVF_To_DXP_info.pb.h>
+#include "MQTTClient.h"
 
-class paho_test_utils
-{
-public:
-    paho_test_utils();
-};
+using namespace std;
+using namespace ford::av::taas;
+
+int publish(const string &PAYLOAD, const string &CLIENTID, const string &TOPIC, const int &QOS, const unsigned int &TIMEOUT);
 
 #endif // PAHO_TEST_UTILS_H
